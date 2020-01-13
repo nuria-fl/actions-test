8dcf508e68f742ed17fb188080d85116b77e01e1
Nuria <nuria@codegram.com>
2020-01-13 16:25:35 +0100

    repo name with owner

--- utils/actions/process-input.js
+++ utils/actions/process-input.js
@@ -3,6 +3,6 @@ const fs = require("fs");
 let data = require(process.env.GITHUB_EVENT_PATH);
 
 fs.writeFileSync(
-  `data/submissions/${data.client_payload.user}-${data.client_payload.user}.json`,
+  `data/submissions/${data.client_payload.repo.replace("/", "_")}.json`,
   JSON.stringify(data.client_payload)
 );
