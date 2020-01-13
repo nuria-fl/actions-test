60a95c4da08dc9676ed3937cf7a2c31f8f496fb7
Nuria <nuria@codegram.com>
2020-01-13 10:58:31 +0100

    test

--- utils/actions/process-input.js
+++ utils/actions/process-input.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 
 let data = require(process.env.GITHUB_EVENT_PATH);
-let output = JSON.parse(data.client_payload);
 
 fs.writeFileSync(
   `data/submissions/${data.client_payload.user}-${data.client_payload.user}.json`,
-  JSON.stringify(output)
+  JSON.stringify(data.client_payload)
 );
