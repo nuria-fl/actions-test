2c78e4bea3d3eeed31c7a20b5c4daeb3bade8306
Nuria <nuria@codegram.com>
2020-01-15 16:42:50 +0100

    get repo and submitter

--- utils/actions/validate-license.js
+++ utils/actions/validate-license.js
@@ -1,4 +1,11 @@
 console.log("validate license");
 let data = require(process.env.GITHUB_EVENT_PATH);
 
-console.log(data);
+const title = data.pull_request.title.split(" - ");
+const body = data.pull_request.body;
+
+const repo = title[1];
+const submitter = body.match(/Submitter: (.+?(?=\n))/);
+
+console.log(repo);
+console.log(submitter);
