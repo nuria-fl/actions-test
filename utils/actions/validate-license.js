3dd154ac25665e279a4d4ac33534abe50b987808
Nuria <nuria@codegram.com>
2020-01-16 11:53:56 +0100

    get license

--- utils/actions/validate-license.js
+++ utils/actions/validate-license.js
@@ -1,8 +1,29 @@
+const github = require("@actions/github");
+const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+
 const getInfo = require("./get-pr-info");
 let data = require(process.env.GITHUB_EVENT_PATH);
 
-console.log(data);
-
 const info = getInfo(data);
 
-console.log(info);
+octokit
+  .graphql(
+    `query getLicense($owner: String!, $name: String!) {
+  repository(name: $name, owner: $owner) {
+    licenseInfo {
+      name
+      key
+      spdxId
+      url
+    }
+  }
+}`,
+    {
+      name: info.repo,
+      owner: info.repoOwner
+    }
+  )
+  .then(result => {
+    console.log(result);
+  })
+  .catch(e => console.error(e));
